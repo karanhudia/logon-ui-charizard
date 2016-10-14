c4bb1e39b23144cfcd622d66c5094a034a5f31e6
Karan Hudia <karanhudia@gmail.com>
2016-10-15 00:25:55 +0530

    Intial Commit - Set up npm, webpack and react environment

---
+++ components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            Making a login and register page.
+            Also, i am required to show 'Hello, World!'
+         </div>
+      );
+   }
+}
+
+export default App;
