8b6669b41e57d3a66893fbb6baa9701f52c8fd6d
Karan Hudia <karanhudia@gmail.com>
2016-10-16 02:10:24 +0530

    Added SCSS for material ripple effect for buttons

--- components/MaterialButton.jsx
+++ components/MaterialButton.jsx
@@ -6,10 +6,36 @@ class MaterialButton extends React.Component {
        this.state = { 
            buttonLabel : this.props.title
        };
+       this.handleClick = this.handleClick.bind(this);
+       this.rippleEffect = this.rippleEffect.bind(this);
+   }
+   componentDidMount() {
+       $(this.refs.ripple).on('animationend webkitAnimationEnd oanimationend MSAnimationEnd', function(e) {
+            $(this).removeClass('is-active');
+       });
+   }
+   handleClick(event) {
+       this.rippleEffect(event);
+   }
+   rippleEffect(event) {
+        var $offset = $(this.refs.ripple.offsetParent).offset();
+        var x = event.pageX - $offset.left;
+        var y = event.pageY - $offset.top;
+        
+        $(this.refs.rippleCircle).css({
+            top: y + 'px',
+            left: x + 'px'
+        });
+        $(this.refs.ripple).addClass('is-active');
    }
    render() {
       return (
-         <button className="material-button">{this.state.buttonLabel}</button>
+          <button onClick={(event) => this.handleClick(event)} className="material-button material-button--primary" type="button">
+             <div ref="ripple" className="material-button-ripple material-button__js-ripple">
+                <span ref="rippleCircle" className="material-button-ripple__circle"></span>
+             </div>
+             {this.state.buttonLabel}
+          </button>
       );
    }
 }
